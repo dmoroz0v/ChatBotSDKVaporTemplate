16b044d0356e79023baafb596b7020ad486bed2f
kramig <kramig@yandex-team.ru>
2024-11-11 11:30:40 +0500

    fix

--- make_cert.sh
+++ make_cert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+readonly PATH_TO_CURR_DIR=`dirname $0`
+
 source .env
 
 rm -f Cert/key.pem
 rm -f Cert/cert.pem
 
-(cd Cert; openssl req -newkey rsa:2048 -sha256 -nodes -keyout key.pem -x509 -days 3650 -out cert.pem -subj "/C=$CERT_COUNTRY/ST=$CERT_CITY/L=$CERT_CITY/O=$CERT_COMPANY/OU=$CERT_DEPARTMENT/CN=$BOT_DOMAIN")
+(cd $PATH_TO_CURR_DIR/Cert; openssl req -newkey rsa:2048 -sha256 -nodes -keyout key.pem -x509 -days 3650 -out cert.pem -subj "/C=$CERT_COUNTRY/ST=$CERT_CITY/L=$CERT_CITY/O=$CERT_COMPANY/OU=$CERT_DEPARTMENT/CN=$BOT_DOMAIN")
